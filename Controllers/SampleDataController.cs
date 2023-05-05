7c49a073522b953112e33b5ac2d5c7f2b220f225
Mpho Jele <mphoj@payspace.com>
2023-05-05 12:17:30 +0200

    Swith view to be about Adventure Time

--- Controllers/SampleDataController.cs
+++ Controllers/SampleDataController.cs
@@ -1,21 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using AdventureTime.Models;
-using DevExtreme.AspNet.Data;
-using DevExtreme.AspNet.Mvc;
-using Microsoft.AspNetCore.Mvc;
-
-namespace AdventureTime.Controllers {
+namespace AdventureTime.Controllers
+{
+    using AdventureTime.Models;
+    using DevExtreme.AspNet.Data;
+    using DevExtreme.AspNet.Mvc;
+    using Microsoft.AspNetCore.Mvc;
 
     [Route("api/[controller]")]
-    public class SampleDataController : Controller {
-
+    public class SampleDataController : Controller
+    {
         [HttpGet]
-        public object Get(DataSourceLoadOptions loadOptions) {
-            return DataSourceLoader.Load(SampleData.Orders, loadOptions);
+        public object Get(DataSourceLoadOptions loadOptions)
+        {
+            return DataSourceLoader.Load(SampleData.Characters, loadOptions);
         }
 
     }
---
+++ Models/SampleCharacter.cs
@@ -0,0 +1,15 @@
+namespace AdventureTime.Models
+{
+    public class SampleCharacter
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Species { get; set; }
+
+        public string Gender { get; set; }
+    }
+}
